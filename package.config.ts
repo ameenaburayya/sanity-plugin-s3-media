10def7f31ce2557a65a31737f121998243512641
Ameen <ameen.aburayya@superside.com>
2026-02-04 21:51:42 +0300

    build: use react compiler

--- package.config.ts
+++ package.config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from '@sanity/pkg-utils'
 export default defineConfig({
   dist: 'dist',
   tsconfig: 'tsconfig.dist.json',
+  babel: {reactCompiler: true},
 
   // Remove this block to enable strict export validation
   extract: {

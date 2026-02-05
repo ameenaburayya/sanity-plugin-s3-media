19a6ec60b2abf196c84e03ef9c2fca7afe16b37a
Ameen <ameen.aburayya@superside.com>
2026-02-05 13:03:14 +0300

    feat: split package

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,4 @@
 import type {S3FileDefinition, S3ImageDefinition} from './types'
-import {buildS3FileUrl, buildS3ImageUrl} from './utils/asset/paths'
-import {
-  getS3AssetExtension,
-  getS3ImageDimensions,
-  tryGetS3AssetExtension,
-  tryGetS3ImageDimensions,
-} from './utils/resolve'
-
-export {createS3Client, S3Client} from './lib'
-
-export {
-  buildS3FileUrl,
-  buildS3ImageUrl,
-  getS3AssetExtension,
-  getS3ImageDimensions,
-  tryGetS3AssetExtension,
-  tryGetS3ImageDimensions,
-}
 
 export {s3Media} from './plugin'
 export type {S3ImageDefinition, S3FileDefinition}
